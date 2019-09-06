6a175d2df1d2612a69e465c284e1ddc3f1b016c6
Haythem Sellami <sellamihaythem96@gmail.com>
2019-09-06 16:38:55 +0100

    Trojan DAO addresses

--- buidler.config.js
+++ buidler.config.js
@@ -19,16 +19,16 @@ module.exports = {
       url: `https://ropsten.infura.io/v3/${INFURA_API_KEY}`,
       accounts: [ ROPSTEN_PRIVATE_KEY ],
       deployedContracts: {
-        moloch: "",
-        pool: ""
+        moloch: '0xa9566CA3Fb25a45dafc978D9FFc3bc6a148EA7E6',
+        pool: '0x35ac1b6df3cEC340476bd59BeEA30d80e8388bDb'
       }
     },
     mainnet: {
       url: `https://mainnet.infura.io/v3/${INFURA_API_KEY}`,
       accounts: [ MAINNET_PRIVATE_KEY ],
       deployedContracts: {
-        moloch: '0x1fd169A4f5c59ACf79d0Fd5d91D1201EF1Bce9f1', // The original Moloch
-        pool: ""
+        moloch: '0xcc7dcdb700eed457c8180406d7d699877f4eee24',
+        pool: ''
       }
     },
     coverage: {
--- deployment-params.js
+++ deployment-params.js
@@ -1,10 +1,10 @@
 // Deployment params
-module.exports.SUMMONER = ''
-module.exports.TOKEN = ''
+module.exports.SUMMONER = '0x4D99d767477Fbb2B47EFeb17E2a78970AD22CCc1'
+module.exports.TOKEN = '0xc778417E063141139Fce010982780140Aa0cD5Ab'
 module.exports.PERIOD_DURATION_IN_SECONDS = 17280
 module.exports.VOTING_DURATON_IN_PERIODS = 35
 module.exports.GRACE_DURATON_IN_PERIODS = 35
 module.exports.ABORT_WINDOW_IN_PERIODS = 5
-module.exports.PROPOSAL_DEPOSIT = '1000000000000000000' // Large numbers should be string or big numbers
+module.exports.PROPOSAL_DEPOSIT = '300000000000000000' // Large numbers should be string or big numbers
 module.exports.DILUTION_BOUND = 3
-module.exports.PROCESSING_REWARD = '10000000000'
+module.exports.PROCESSING_REWARD = '30000000000000000'
